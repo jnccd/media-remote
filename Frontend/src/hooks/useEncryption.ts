575adc0330fe982214637675aea9165bab4a0a06
Dobiko <kobidogao@yahoo.com>
2025-08-16 20:41:23 +0200

    Add random padding

--- Frontend/src/hooks/useEncryption.ts
+++ Frontend/src/hooks/useEncryption.ts
@@ -7,10 +7,23 @@ function xorCipher(text: string, key: string): string {
   return output;
 }
 
+function randomAsciiString(length: number): string {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const idx = Math.floor(Math.random() * chars.length);
+    result += chars[idx];
+  }
+  return result;
+}
+
 export function getAuthHeaderForMediaControlAPI(password: string) {
   const isoNow = new Date().toISOString();
 
-  const plaintext = `abc,${isoNow}`;
+  const plaintext = `${randomAsciiString(
+    Math.floor(Math.random() * 7) + 2
+  )},${isoNow}`;
 
   return btoa(xorCipher(plaintext, password));
 }
