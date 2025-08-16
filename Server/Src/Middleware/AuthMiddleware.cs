a5a0caa3fd3efef03c402487bfb604b461b9a328
Dobiko <kobidogao@yahoo.com>
2025-08-16 22:51:07 +0200

    Add middleware logging

--- Server/Src/Middleware/AuthMiddleware.cs
+++ Server/Src/Middleware/AuthMiddleware.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Server.Helpers;
+using Server.Services;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,10 @@ using System.Threading.Tasks;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
 public class CustomAuthorizeAttribute : Attribute { }
 
-public class AuthMiddleware
+public class AuthMiddleware(RequestDelegate next, IConfiguration config, LoggerService logger)
 {
-    private readonly RequestDelegate _next;
-    private readonly string _expectedPassword;
-
-    public AuthMiddleware(RequestDelegate next, IConfiguration config)
-    {
-        _next = next;
-        _expectedPassword = config["PASSWORD"] ?? "very-secure-standard-password";
-    }
+    private readonly RequestDelegate _next = next;
+    private readonly string _expectedPassword = config["PASSWORD"] ?? "pass";
 
     record CustomHttpHeader(CustomHttpHeaders headers);
     record CustomHttpHeaders(string Authorization);
@@ -47,18 +42,21 @@ public class AuthMiddleware
         {
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
             await context.Response.WriteAsync("Unauthorized: Missing or malformed Authorization header.");
+            logger.WriteLine("Unauthorized: Missing or malformed Authorization header.");
             return;
         }
         if (!DateTime.TryParse(decryptedDateTimeString, null, System.Globalization.DateTimeStyles.RoundtripKind, out var decryptedDateTime))
         {
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
             await context.Response.WriteAsync("Unauthorized: Invalid Authorization.");
+            logger.WriteLine("Unauthorized: Invalid Authorization.");
             return;
         }
-        if (decryptedDateTime < DateTime.UtcNow.AddSeconds(-2) || decryptedDateTime > DateTime.UtcNow.AddSeconds(1))
+        if (decryptedDateTime < DateTime.UtcNow.AddSeconds(-2) || decryptedDateTime > DateTime.UtcNow.AddSeconds(3))
         {
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
             await context.Response.WriteAsync("Unauthorized: Outdated Authorization.");
+            logger.WriteLine($"Unauthorized: Outdated Authorization. by {decryptedDateTime - DateTime.UtcNow}");
             return;
         }
 
