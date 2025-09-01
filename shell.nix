06ba0b0c545762a98218212920014af3994fa34f
Dobiko <kobidogao@yahoo.com>
2025-09-01 18:05:38 +0200

    Add pnpm req

--- shell.nix
+++ shell.nix
@@ -2,4 +2,4 @@
 # export NIXPKGS_ALLOW_INSECURE=1 && nix-shell --impure --command "bash update_and_start.sh"
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
-mkShell { packages = [ icu dotnet-sdk dotnet-ef npm nodejs_22 ]; }
+mkShell { packages = [ icu dotnet-sdk dotnet-ef pnpm npm nodejs_22 ]; }
